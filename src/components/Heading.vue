5088135d878fae427a1afadf00d30bda72b6c83a
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-11-12 14:43:17 -0600

    renaming some components

---
+++ src/components/Heading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>NFL 4th Down Calculator</h1>
+    <p>
+      Watch out, Nagy!
+      A guide to help you determine the optimal course of action for 4th Down situations<br>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Heading'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

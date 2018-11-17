779a32fbfba6bde56e9cceb3fd774efcf52ffebe
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-11-17 17:20:06 -0600

    adding endzones

--- src/components/FootballField.vue
+++ src/components/FootballField.vue
@@ -12,9 +12,18 @@ export default {
       let ctx = canvas.getContext('2d')
       ctx.fillStyle = 'green'
       ctx.fillRect(0, 0, canvas.width, canvas.height)
-      ctx.beginPath()
-      ctx.arc(95, 50, 40, 0, 2 * Math.PI)
-      ctx.stroke()
+      ctx.fillStyle = 'blue'
+      ctx.fillRect(0, 0, 40, 216)
+      ctx.fillRect(440, 0, 480, 216)
+      ctx.strokeStyle='white'
+      // #013220
+
+      for (let i = 2; i < 23; i++) {
+        ctx.beginPath()
+        ctx.moveTo(20*i, 0)
+        ctx.lineTo(20*i ,216)
+        ctx.stroke()
+      }
     }
   },
   mounted: function (){

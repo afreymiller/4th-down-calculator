4fc9f9f6598a4b299de19e874bb9fdc149413e05
Adam Freymiller <ADAM.FREYMILLER@morningstar.com>
2018-11-17 15:45:27 -0600

    changing column alignment

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="team-border row">
-    <div class="col-4">
+    <div class="col-3">
       <img 
         height="50px"
         width="50px"
@@ -8,8 +8,24 @@
       />
       <p>{{name}}</p>
     </div>
-    <div class="col-2">
-      <p>Other stats</p>
+    <div class="col-1">
+      <p>YPP:</p>
+      <p>EP:</p>
+    </div>
+    <div class="col-3">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70"
+        aria-valuemin="0" aria-valuemax="100" style="width:70%">
+          <span class="sr-only">70% Complete</span>
+        </div>
+      </div>
+      <br>
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="70"
+        aria-valuemin="0" aria-valuemax="100" style="width:70%">
+          <span class="sr-only">70% Complete</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,11 +38,6 @@ export default {
       default: ''
     }
   },
-  // data () {
-  //   return {
-  //     srcPath: require('../assets/' + this.name + '.png')
-  //   }
-  // }
   computed: {
     srcPath: function () {
       return require('../assets/' + this.name + '.png')
